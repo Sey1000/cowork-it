7bbb77142c0aaf7223fe613e8b159cc8eb111c32
Kristofer Espinosa <kristofer.espinosa@gmail.com>
2017-08-21 17:02:01 +0200

    correction small bug

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   end
 
   resources :bookings
-  resources :users, only [:index, :show]
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
