248913b99990de2ff30932a5ef7fcc66049c2816
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-22 14:35:08 +0200

    Clean worktree before pull

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -2,4 +2,8 @@ class Desk < ApplicationRecord
   belongs_to :coffeeshop
   has_many :bookings
   has_many :users, through: :bookings
+
+  def str_date
+    self.start_time.strftime("%b %d")
+  end
 end
