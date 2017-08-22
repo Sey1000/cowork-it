0a0a900aa7cd5071a7b18eb1d00529a45f7cf83f
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-22 18:19:10 +0200

    Clear working tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,12 +7,18 @@
 //= require_tree .
 
 $(document).ready(function() {
+    // this array is for start, end time of booking
+    var time_arr = []
   $('.time-select-btn').click(function() {
-    var time_int = $(this).data('id');
+    var time_obj = $(this).data('time');
     var desk_id = $(this).parents().eq(2).data('id');
+    var desk_date = $(this).data('date')
+
     $('#hidden-coffeeshop-name').val(desk_id);
-    $('#hidden-start-time').val(time_int);
-    console.log(time_int);
+    $('#hidden-start-time').val(time_obj);
+    $('#booking-desk-date').text(desk_date);
+    console.log(time_obj);
     console.log(desk_id);
+    console.log(desk_date);
   });
 });
