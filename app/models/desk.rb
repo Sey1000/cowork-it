6a6150398612bb3d29eb8d0ba8b4c6db9f8e901c
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-22 14:59:18 +0200

    Clear worktree before pull

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -4,6 +4,10 @@ class Desk < ApplicationRecord
   has_many :users, through: :bookings
 
   def str_date
-    self.start_time.strftime("%b %d")
+    self.start_time.strftime("%b %d %a")
+  end
+
+  def hour_count
+    self.end_time.hour - self.start_time.hour
   end
 end
