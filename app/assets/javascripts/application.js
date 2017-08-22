2dc4c82f4890a4c0da9f9a59f7ae1405c9aef9a5
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-22 18:26:24 +0200

    Complete JS to show booking summary

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,17 +8,21 @@
 
 $(document).ready(function() {
     // this array is for start, end time of booking
-    var time_arr = []
   $('.time-select-btn').click(function() {
     var time_obj = $(this).data('time');
     var desk_id = $(this).parents().eq(2).data('id');
-    var desk_date = $(this).data('date')
+    var desk_date = $(this).data('date');
+    var start = $(this).data('start');
+    var end = $(this).data('end');
 
     $('#hidden-coffeeshop-name').val(desk_id);
     $('#hidden-start-time').val(time_obj);
     $('#booking-desk-date').text(desk_date);
+    $('#booking-time-range').text(start + " - " + end);
     console.log(time_obj);
     console.log(desk_id);
     console.log(desk_date);
+    console.log(start);
+    console.log(end);
   });
 });
