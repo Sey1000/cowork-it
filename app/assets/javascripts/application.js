818360e58e947b636ff61f8cfce7dd8af9af2994
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-22 16:05:32 +0200

    clear worktree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,10 @@
 //= require underscore
 //= require gmaps/google
 //= require_tree .
+
+$(document).ready(function() {
+  $('.time-select-btn').click(function() {
+    console.log($(this).data('id'));
+    console.log($(this).parents().eq(2).data('id'));
+  });
+});
