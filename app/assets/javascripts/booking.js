9f60cb2c81a53777d2ed41ea308f5e3c37c8d3aa
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-23 14:44:17 +0200

    Fix bug in create

--- app/assets/javascripts/booking.js
+++ app/assets/javascripts/booking.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // this array is for start, end time of booking
   $('.time-select-btn').click(function() {
     var time_obj = $(this).data('time');
-    var desk_id = $(this).parents().eq(2).data('id');
+    var desk_id = $(this).parents().eq(3).data('id');
     var desk_date = $(this).data('date');
     var start = $(this).data('start');
     var end = $(this).data('end');
@@ -15,10 +15,7 @@ $(document).ready(function() {
     $('#hidden-end-time').val(end_time_obj);
     $('#booking-desk-date').text(desk_date);
     $('#booking-time-range').text(start + " - " + end);
-    console.log(time_obj);
-    console.log(desk_id);
-    console.log(start_time_obj);
-    console.log(end_time_obj);
-
+    console.log($(this));
+    console.log($(this).parents().eq(3).data('id'));
   });
 });
