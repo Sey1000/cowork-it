c979366711a34eb3ae3cc9d177e584c5efd08693
Nhung Nguyen <pn.nguyen@outlook.com>
2017-08-23 15:08:04 +0200

    change button design

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -21,7 +21,7 @@ Geocoder.configure(
   # distances: :linear          # :spherical or :linear
 
   :lookup    => :google,
-  :api_key   => ENV['GOOGLE_API_SERVER_KEY'],
+  # :api_key   => ENV['GOOGLE_API_SERVER_KEY'],
   :use_https => true
   # [...]
 )
