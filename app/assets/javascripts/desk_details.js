6dd08b9045773dc5006e5c985bba5f03bc94065e
Nhung Nguyen <pn.nguyen@outlook.com>
2017-08-23 14:26:39 +0200

    toggle function

--- app/assets/javascripts/desk_details.js
+++ app/assets/javascripts/desk_details.js
@@ -1,9 +1,7 @@
 $(function() {
-  function toggleContent() {
-    // Get the DOM reference
-    var contentId = document.getElementById("content");
-    // Toggle
-    contentId.style.display == "block" ? contentId.style.display = "none" :
-  contentId.style.display = "block";
-  }
+  $('.desk-head').on('click', function(event) {
+      $(this).siblings('.desk-content').toggleClass('hidden');
+      console.log(this);
+  });
 })
+
