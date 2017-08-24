f96a9bb0a8b2292f840c4695cb7687c69e5e52c8
Kristofer Espinosa <kristofer.espinosa@gmail.com>
2017-08-24 14:47:47 +0200

    still there

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   before_create :set_photo
   has_attachment :photo
 
