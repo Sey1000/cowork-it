10043a5a527eb510fba57c829857f1ee248b0f99
Maren Wehrheim <marenwehrheim@hotmail.de>
2017-08-24 15:15:08 +0200

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -85,8 +85,6 @@ ActiveRecord::Schema.define(version: 20170824105812) do
     t.string   "facebook_picture_url"
     t.string   "token"
     t.datetime "token_expiry"
-    t.float    "latitude"
-    t.float    "longitude"
     t.string   "photo_url"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
