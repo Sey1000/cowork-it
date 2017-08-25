e2c08d3f8fdfba69897aa3073b5facfa593f96f9
Maren Wehrheim <marenwehrheim@hotmail.de>
2017-08-25 14:48:05 +0200

    search

---
+++ app/assets/javascripts/channels/navbar.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $(window).scroll(function () {
+    //if you hard code, then use console
+    //.log to determine when you want the
+    //nav bar to stick.
+    console.log($(window).scrollTop())
+    if ($(window).scrollTop() < 728) {
+      $('.navbar-wagon').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() > 729) {
+      $('.navbar-wagon').removeClass('navbar-fixed');
+    }
+  });
+});
